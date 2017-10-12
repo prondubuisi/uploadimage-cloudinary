f67df6cbe43bb0b436bc88a5639a85d2c3f16f3d
NdubuisiO <onyemenamndu@gmail.com>
2017-10-12 11:56:29 +0100

    on the way again

---
+++ imageupload.js
@@ -0,0 +1,16 @@
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#fileToUpload").change(function(){
+    readURL(this);
+});
---
+++ imageupload.php
@@ -0,0 +1,55 @@
+
+<?php
+
+
+require 'Cloudinary.php';
+require 'Uploader.php';
+require 'Api.php';
+
+
+\Cloudinary::config(array( 
+  "cloud_name" => "dnuraq8oa", 
+  "api_key" => "274545435744652", 
+  "api_secret" => "jflXJqU3PYSSqtRUAoVz3F1D4dU" 
+));
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+
+    $image_properties = \Cloudinary\Uploader::upload($_FILES["fileToUpload"]["tmp_name"]);
+            $image_url =$image_properties['secure_url'];
+            echo('           '.$image_url);
+            return true;
+}
+?>
